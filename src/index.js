5c64fdd8135e34365a7c7ba8593c02af8240475c
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2021-04-27 07:31:05 +0530

    added color to the para 2

--- src/index.js
+++ src/index.js
@@ -4,25 +4,67 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-var time = new Date().toLocaleTimeString({hour:'numeric',hour12:false});
-var greet;
-if(time>=1&&time<=11)
+
+var para1 =
 {
-greet="Good Morning"
-}if(time>=12&&time<=7)
+  textAlign: "center",
+  fontSize: "3.9rem",
+  color:"#f07b2d",
+  display: "inline-block",
+  fontWeight: "649",
+  marginTop:"35px",
+ 
+
+}
+
+var para2 =
+{
+  textAlign:"center" ,
+  fontSize:"3.9rem" ,
+  color:"#50d305" ,
+  display:"inline-block" ,
+  fontWeight:"649" ,
+  marginTop:"35px" ,
+  
+
+}
+
+
+var firstdiv =
+{
+backgroundColor: "#ffec94",
+marginTop:"250px",
+textAlign: "center",
+height: "174px",
+width: "925px",
+borderRadius: "45px",
+}
+
+
+var time = new Date().getHours({hour:'numeric',hour12:false});
+var greet;
+
+if(time>=1 && time<=11)
+{
+greet="Good Morning";
+
+}if(time>=12 && time<=19)
 {
   greet="Good Afternoon"
-}else{
-  greet = "Good Night"
+  para2.color="orange";
+
+}if(time>=19){
+  greet = "Good Night";
+  para2.color="black"
 }
 
 ReactDOM.render(
   <React.StrictMode>
     <>
     <center>
-    <div className="firstdiv">
-      <p className="para1">Hello Sir,</p>&nbsp;&nbsp;
-      <p className="para2">{greet}</p>
+    <div style={firstdiv}>
+      <p id="paraone"style={para1}>Hello Sir,</p>&nbsp;&nbsp;
+      <p id="paratwo" style={para2}>{greet}</p>
     </div>
     </center>
     </>
